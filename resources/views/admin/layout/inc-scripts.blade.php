504b2ad4a6355a8e064de8c49f979db559d9d176
jobkung3d <crystan_job.a@hotmail.com>
2018-01-30 21:45:31 +0700

    Update

---
+++ resources/views/admin/layout/inc-scripts.blade.php
@@ -0,0 +1,8 @@
+
+<script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('bower_components/jquery/dist/jquery.min.js') }}"></script><!-- jQuery 3 -->
+<!-- <script type="text/javascript" src="{{ URL::asset('bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script><!-- Bootstrap 3.3.7 --> -->
+<script type="text/javascript" src="{{ URL::asset('bower_components/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script><!-- SlimScroll -->
+<script type="text/javascript" src="{{ URL::asset('bower_components/fastclick/lib/fastclick.js') }}"></script><!-- FastClick -->
+<script type="text/javascript" src="{{ URL::asset('dist/js/adminlte.min.js') }}"></script><!-- AdminLTE App -->
+<!-- <script type="text/javascript" src="{{ URL::asset('dist/js/demo.js') }}"></script><!-- AdminLTE for demo purposes --> -->
---
+++ resources/views/admin/layout/template.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>AdminLTE 2 | Top Navigation</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+  @include('admin.layout.inc-stylesheet')
+  @yield('stylesheet')
+</head>
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+
+  <!-- /.navbar-header -->
+  @include('admin.layout.inc-header')
+  <!-- /.navbar-top-links -->
+
+
+  <!-- Full Width Column -->
+  <div id="app"></div>
+  <div class="content-wrapper">
+    <div class="container">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          Top Navigation
+          <small>Example 2.0</small>
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li><a href="#">Layout</a></li>
+          <li class="active">Top Navigation</li>
+        </ol>
+      </section>
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="callout callout-info">
+          <h4>Tip!</h4>
+
+          <p>Add the layout-top-nav class to the body tag to get this layout. This feature can also be used with a
+            sidebar! So use this class if you want to remove the custom dropdown menus from the navbar and use regular
+            links instead.</p>
+        </div>
+        <div class="callout callout-danger">
+          <h4>Warning!</h4>
+
+          <p>The construction of this layout differs from the normal one. In other words, the HTML markup of the navbar
+            and the content will slightly differ than that of the normal layout.</p>
+        </div>
+        <div class="box box-default">
+          <div class="box-header with-border">
+            <h3 class="box-title">Blank Box</h3>
+          </div>
+          <div class="box-body">
+            The great content goes here
+          </div>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.container -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+  @include('admin.layout.inc-footer')
+  @yield('scripts')
+<!-- ./wrapper -->
+
+@include('admin.layout.inc-scripts')
+@yield('scripts')
+</body>
+</html>
